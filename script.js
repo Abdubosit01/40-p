45000a954ed22295e3d1e601e6064cca894cb03f
Abdubosit01 <abdubosit0115@gmail.com>
2025-07-30 14:46:49 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+let data = ['uzum', 'banan','olma','anor',]
+
+
+console.log(data);
+
+
+
+
+
+let bir =[]
+
+let ikki =[]
+
+for( item of data){
+
+if(item.includes('n')){
+bir.push(item)
+
+}else{
+    ikki.push(item)
+}
+
+}
+
+
+
+
+
+console.log(bir);
+
+
+console.log(ikki);
+
+
+
+
+
+
+
+
+
